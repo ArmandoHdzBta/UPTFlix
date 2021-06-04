08108c5a0c0cbea1e3b5fd0ba1c69da8b9b2a1d2
Armando Hernandez <armando_1319104150@uptecamac.edu.mx>
2021-06-04 10:20:40 -0500

    Layaout js css empty

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,9 +8,11 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     {{-- BOOTSTRAP CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    {{-- CSS IF NEED --}}
+    @yield('css')
     <title>@yield('title', "UPTFlix")</title>
 </head>
-<body class="bg-dark">
+<body class="bg-dark text-white">
     <!-- NAV BAR -->
 	<nav class="navbar navbar-expand-sm navbar-dark bg-dark ml-0 col-12">
 		<div class="container-fluid">
@@ -36,6 +38,8 @@
     <!-- SCRIPTS BOOTSTRAP -->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+	{{-- JS IF NEED --}}
+	@yield('js')
     <!-- ANIMATION SCRIPT -->
 	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 	<script>
