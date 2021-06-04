2fb2af6e48045d6e786ebf280c644e560e38be34
Armando Hernandez <armando_1319104150@uptecamac.edu.mx>
2021-06-04 12:51:40 -0500

    Validation and Model registro

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,5 +1,24 @@
 @extends('layout')
 
+@section('css')
+	<style>
+		input[type="text"],
+		input[type="email"],
+		input[type="password"]{
+			background: transparent;
+			color: #fff;
+			border: none;
+			border-bottom: 2px solid #067a22;
+		}
+		input[type="text"]:focus,
+		input[type="email"]:focus,
+		input[type="password"]:focus{
+			background: none;
+			color: #fff;
+		}
+	</style>
+@endsection
+
 @section("Registo")
 
 @section('content')
@@ -16,41 +35,65 @@
 						</div>
 					@endif
 				@endif
-				<form action="{{ route('signup') }}" method="POST">
+				<form action="{{ route('signup') }}" id="signup" method="POST">
 					@csrf
-					<div class="form-group">
-						<label for="" class="form-control">Nombre</label>
+					<div class="form-group mb-2">
+						<label for="" class="form-control bg-transparent text-white">Nombre</label>
 						<input type="text" required class="form-control" name="nombre" placeholder="Nombre">
 					</div>
-					<div class="row">
+					<div class="row mb-2">
 						<div class="form-group col-6">
-							<label for="" class="form-control">Apellido paterno</label>
+							<label for="" class="form-control bg-transparent text-white">Apellido paterno</label>
 							<input type="text" required class="form-control" name="app" placeholder="Apellido paterno">
 						</div>
 						<div class="form-group col-6">
-							<label for="" class="form-control">Apellido materno</label>
+							<label for="" class="form-control bg-transparent text-white">Apellido materno</label>
 							<input type="text" required class="form-control" name="apm" placeholder="Apellido Materno">
 						</div>
 					</div>
-					<div class="form-group">
-						<label for="" class="form-control">Correo</label>
+					<div class="form-group mb-2">
+						<label for="" class="form-control bg-transparent text-white">Correo</label>
 						<input type="email" required class="form-control" name="email" placeholder="ejemplo@gmail.com">
+						@error('email')
+							<small class="text-danger">
+								* Debe de ser un correo válido
+							</small>
+						@enderror
 					</div>
-					<div class="form-group">
-						<label for="" class="form-control">Dirección</label>
+					<div class="form-group mb-2">
+						<label for="" class="form-control bg-transparent text-white">Dirección</label>
 						<input type="text" required class="form-control" name="address" placeholder="Direccion">
 					</div>
-					<div class="form-group">
-						<label for="" class="form-control">Contraseña</label>
-						<input type="password" required class="form-control" name="pass1" placeholder="Contraseña">
-					</div>
-					<div class="form-group">
-						<label for="" class="form-control">Repetir contraseña</label>
-						<input type="password" required class="form-control" name="pass2" placeholder="Repite la contraseña">
+					<div class="row mb-2">
+						<div class="form-group col-6">
+							<label for="" class="form-control bg-transparent text-white">Contraseña</label>
+							<input type="password" required class="form-control" name="pass1" placeholder="Contraseña">
+						</div>
+						<div class="form-group col-6">
+							<label for="" class="form-control bg-transparent text-white">Repetir contraseña</label>
+							<input type="password" required class="form-control" name="pass2" placeholder="Repite la contraseña">
+						</div>
 					</div>
 					<input type="submit" class="btn btn-success col-12" name="enviar" value="Crear cuenta">
 				</form>
 			</div>
 		</div>
 	</div>
+@endsection
+
+@section('js')
+	<script>
+		$(document).ready(function() {
+			$.ajax({
+				url: '',
+				type: 'default GET (Other values: POST)',
+				dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+				data: {param1: 'value1'},
+			})
+			.done(function() {
+				console.log("success");
+			});
+
+		});
+	</script>
 @endsection
