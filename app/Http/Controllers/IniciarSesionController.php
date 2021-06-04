07b01af245bba6125da3535f83a7728f60d86dd8
Armando Hernandez <armando_1319104150@uptecamac.edu.mx>
2021-06-04 13:27:57 -0500

    Route iniciar sesion

---
+++ app/Http/Controllers/IniciarSesionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IniciarSesionController extends Controller
+{
+    public function iniciarsesion()
+    {
+    	//vista
+    }
+
+    public function signin(Request $datos)
+    {
+    	# code...
+    }
+}
